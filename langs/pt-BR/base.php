fadaaac62f8fadb73890d1ec4325d72be505fff4
Bruno Goncalves <bugotech@gmail.com>
2017-07-09 03:53:39 -0300

    base

--- langs/pt-BR/base.php
+++ langs/pt-BR/base.php
@@ -6,7 +6,7 @@ return [
         'subtitle' => 'Vamos criar um novo batalhão?',
         'message' => '<h4>Estamos quase lá!</h4>
             Abaixo, você deve escolher em que região deseja que seu batalhão seja estabelecido.<br>
-            <b>ATENÇÃO:</b> Caso você já tinha um batalhão e o jogo esta solicitando a criação de um novo, é por que você ficou muito tempo sem jogar<br>
-            ou por que seu batalhão foi invadido e dominado por outro jogador.',
+            <div class="alert alert-warning"><b>ATENÇÃO:</b> Caso você já tinha um batalhão e o jogo esta solicitando a criação de um novo, é por que você ficou muito tempo sem jogar
+            ou por que seu batalhão foi invadido e dominado por outro jogador.</div>',
     ],
 ];
