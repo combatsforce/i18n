685e47b4cad35e691f1df00e0ac516929bb0d226
Bruno Goncalves <bugotech@gmail.com>
2017-07-09 04:11:41 -0300

    map

---
+++ langs/pt-BR/map.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    "regions" => [
+        "N"  => "Norte",
+        "S"  => "Sul",
+        "W"  => "Oeste",
+        "E"  => "Leste",
+        "NE" => "Nordeste",
+        "NW" => "Noroeste",
+        "SE" => "Sudeste",
+        "SW" => "Sudoeste",
+        "C"  => "Centro",
+    ],
+];
