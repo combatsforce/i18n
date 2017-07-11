95f82d610084b952a67517b83d182ad3bd3901e5
Bruno Goncalves <bugotech@gmail.com>
2017-07-11 15:53:14 -0300

    builds

--- langs/pt-BR/builds.php
+++ langs/pt-BR/builds.php
@@ -4,7 +4,7 @@ return [
     'injob-description' => 'Construir :build...',
 
     'mainroom' => [
-        'name' => 'Sala principal',
-        'description' => 'Na Sala Principal você pode construir outros edifícios.',
+        'name' => 'Sala da administração',
+        'description' => 'Na sala de administração é onde você pode controlar e gerenciar outros edifícios de seu batalhão.',
     ],
 ];
